354eeb7e6299d8953f2631650721953dcfd21f41
Daria Abdulnasyrova <abdulnasyrova@competentum.ru>
2019-08-12 17:09:39 +0300

    Add deploy task

--- build/cg-slider.min.js
+++ build/cg-slider.min.js
@@ -1,7 +1,7 @@
 /*!
  * cg-slider v0.1.8 - Accessible Slider Component
  * 
- * (c) 2015-2018 Competentum Group | http://competentum.com
+ * (c) 2015-2019 Competentum Group | http://competentum.com
  * Released under the MIT license
  * https://opensource.org/licenses/mit-license.php
  */
